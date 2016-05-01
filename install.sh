d1cad2ba9cd8b939a740330f67d7ebbb6e21db8c
Jeffrey Crowell <crowell@bu.edu>
2016-04-30 22:36:23 -0400

    make these not . files in here
    so i can see them by default on my laptop

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ln -s `pwd`/.tmux.conf ~/.tmux.conf
-ln -s `pwd`/.vimrc ~/.vimrc
-ln -s `pwd`/.radare2rc ~/.radare2rc
-ln -s `pwd`/.gitconfig ~/.gitconfig
+ln -s `pwd`/tmux.conf ~/.tmux.conf
+ln -s `pwd`/vimrc ~/.vimrc
+ln -s `pwd`/radare2rc ~/.radare2rc
+ln -s `pwd`/gitconfig ~/.gitconfig
